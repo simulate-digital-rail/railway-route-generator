63b22bcdc51f4e7f178bf85c0f720497b05baf0d
Julian B <jeriox@users.noreply.github.com>
2022-12-06 15:08:14 +0100

    fix tests

--- test/line_test.py
+++ test/line_test.py
@@ -1,9 +1,12 @@
+from planpro_importer.reader import PlanProReader
+
 from railwayroutegenerator import generator
 from .helper import compare_route_lists
 
 
 def test_line():
-    routes = generator.generate_from_planpro("line-test.ppxml", output_format="python-objects")
+    topology = PlanProReader("line-test.ppxml").read_topology_from_plan_pro_file()
+    routes = generator.generate_from_topology(topology, output_format="python-objects")
     expected_routes = [("60ES", "60AS")]
     compare_route_lists(routes, expected_routes)
 
