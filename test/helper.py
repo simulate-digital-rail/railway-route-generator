d7cd1fcfe697b2800d121c9a3f62f34c86e981be
Julian B <jeriox@users.noreply.github.com>
2023-01-24 13:56:38 +0100

    lint

--- test/helper.py
+++ test/helper.py
@@ -1,5 +1,8 @@
 def _test_single_route(route, expected_route):
-    return route.start_signal.name == expected_route[0] and route.end_signal.name == expected_route[1]
+    return (
+        route.start_signal.name == expected_route[0]
+        and route.end_signal.name == expected_route[1]
+    )
 
 
 def compare_route_lists(generated_routes, expected_routes):
@@ -7,12 +10,14 @@ def compare_route_lists(generated_routes, expected_routes):
         found_any = False
         for route in generated_routes:
             found_any = found_any or _test_single_route(route, expected_route)
-        assert found_any, "Route "+str(expected_route)+" not found."
+        assert found_any, "Route " + str(expected_route) + " not found."
 
     for route in generated_routes:
         found_any = False
         for expected_route in expected_routes:
             found_any = found_any or _test_single_route(route, expected_route)
-        assert found_any, f"Route {route.start_signal.name}-{route.end_signal.name} found but was not expected."
+        assert (
+            found_any
+        ), f"Route {route.start_signal.name}-{route.end_signal.name} found but was not expected."
 
     assert len(generated_routes) == len(expected_routes)
--- test/point-with-other-signal_test.py
+++ test/point-with-other-signal_test.py
@@ -1,15 +1,18 @@
 from planpro_importer.reader import PlanProReader
 
 from railwayroutegenerator import generator
+
 from .helper import compare_route_lists
 
 
 def test_point_with_other_signal():
-    topology = PlanProReader("point-with-other-signal-test.ppxml").read_topology_from_plan_pro_file()
+    topology = PlanProReader(
+        "point-with-other-signal-test.ppxml"
+    ).read_topology_from_plan_pro_file()
     routes = generator.generate_from_topology(topology, output_format="python-objects")
     expected_routes = [("60ES", "60AS")]
     compare_route_lists(routes, expected_routes)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test_point_with_other_signal()
